e7ffb38a8e50a11704b0f4a6de06a37dc538f50b
Chris <firstof9@gmail.com>
2022-04-06 19:08:43 -0700

    fix: add charging_power property

--- openevsehttp/__init__.py
+++ openevsehttp/__init__.py
@@ -769,6 +769,17 @@ class OpenEVSE:
             return self._config["wifi_serial"]
         return None
 
+    @property
+    def charging_power(self) -> float:
+        """Return the charge power.
+
+        Calculate Watts base on V*I
+        """
+        assert self._status is not None
+        value = round(self._status["voltage"] * (self._status["amp"] / 1000),2)
+        return value
+
+
     # There is currently no min/max amps JSON data
     # available via HTTP API methods
     @property
