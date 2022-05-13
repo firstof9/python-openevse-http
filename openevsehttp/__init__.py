1c339675aac9e275053cf9359e1641acf5963763
Chris <firstof9@gmail.com>
2022-05-13 07:15:36 -0700

    fix: fix setting divertmode

--- openevsehttp/__init__.py
+++ openevsehttp/__init__.py
@@ -380,15 +380,15 @@ class OpenEVSE:
             _LOGGER.error("Problem issuing command: %s", response["msg"])
             raise UnknownError
 
-    async def divert_mode(self, mode: str = "Normal") -> None:
+    async def divert_mode(self, mode: str = "normal") -> None:
         """Set the divert mode to either Normal or Eco modes."""
         url = f"{self.url}divertmode"
 
-        if mode != "Normal" or mode != "Eco":
+        if mode not in ["normal", "eco"]:
             _LOGGER.error("Invalid value for divertmode: %s", mode)
             raise ValueError
 
-        if mode == "Normal":
+        if mode == "normal":
             value = 1
         else:
             value = 2
@@ -399,9 +399,12 @@ class OpenEVSE:
         response = await self.process_request(
             url=url, method="post", data=data
         )  # noqa: E501
+        _LOGGER.debug("divert_mode response: %s", response)
         if response["msg"] != "done":
             _LOGGER.error("Problem issuing command: %s", response["msg"])
             raise UnknownError
+        return response
+
 
     async def get_override(self) -> None:
         """Get the manual override status."""
