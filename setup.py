5178ee7d47213496f02d8cb059ba19643915501a
firstof9@gmail.com <firstof9@gmail.com>
2024-03-26 18:08:05 -0700

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 PROJECT_DIR = Path(__file__).parent.resolve()
 README_FILE = PROJECT_DIR / "README.md"
-VERSION = "0.1.59"
+VERSION = "0.1.60"
 
 setup(
     name="python-openevse-http",
