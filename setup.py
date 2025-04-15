0964c7e864bc522d8d57f7548d61021b0a080475
Chris <1105672+firstof9@users.noreply.github.com>
2025-04-15 13:55:56 -0700

    version bump (#431)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 PROJECT_DIR = Path(__file__).parent.resolve()
 README_FILE = PROJECT_DIR / "README.md"
-VERSION = "0.1.78"
+VERSION = "0.1.79"
 
 setup(
     name="python_openevse_http",
