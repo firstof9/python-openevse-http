94346577943529fe6bde815387feff3ed7cb304c
Joost Lekkerkerker <joostlek@outlook.com>
2026-01-05 20:53:05 +0100

    Update install_requires to remove 'requests'
    Removed 'requests' from the install_requires list.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     long_description_content_type="text/markdown",
     packages=find_packages(exclude=["test.*", "tests"]),
     python_requires=">=3.10",
-    install_requires=["aiohttp", "requests"],
+    install_requires=["aiohttp"],
     entry_points={},
     include_package_data=True,
     zip_safe=False,
