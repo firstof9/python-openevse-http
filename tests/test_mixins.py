bc7cffc46fc5c68af48839dcdd076584b8259fa6
firstof9@gmail.com <firstof9@gmail.com>
2026-04-03 08:23:12 -0700

    add unit tests for mixin classes to ensure full coverage

---
+++ tests/test_mixins.py
@@ -0,0 +1,90 @@
+"""Tests for the Mixin classes to ensure 100% coverage."""
+
+import pytest
+
+from openevsehttp.commands import CommandsMixin
+from openevsehttp.managers import ManagersMixin
+from openevsehttp.properties import PropertiesMixin
+from openevsehttp.sensors import SensorsMixin
+
+# ruff: noqa: SLF001
+
+
+class DummyCommands(CommandsMixin):
+    """Dummy class for CommandsMixin tests."""
+
+    url = "http://test"
+    _status = {}
+    _config = {}
+    _session = None
+
+
+class DummyManagers(ManagersMixin):
+    """Dummy class for ManagersMixin tests."""
+
+    url = "http://test"
+
+
+class DummyProperties(PropertiesMixin):
+    """Dummy class for PropertiesMixin tests."""
+
+    _status = {}
+    _config = {}
+
+
+class DummySensors(SensorsMixin):
+    """Dummy class for SensorsMixin tests."""
+
+    url = "http://test"
+
+
+def test_mixins_sync_not_implemented():
+    """Test sync NotImplementedError in all mixins."""
+    for cls in [DummyCommands, DummyManagers, DummyProperties, DummySensors]:
+        obj = cls()
+        with pytest.raises(NotImplementedError):
+            obj._version_check("1.0.0")
+
+
+@pytest.mark.asyncio
+async def test_commands_mixin_async_not_implemented():
+    """Test async NotImplementedError in CommandsMixin."""
+    cmds = DummyCommands()
+    with pytest.raises(NotImplementedError):
+        await cmds.process_request("http://test")
+    with pytest.raises(NotImplementedError):
+        await cmds.send_command("test")
+    with pytest.raises(NotImplementedError):
+        await cmds.update()
+
+
+@pytest.mark.asyncio
+async def test_managers_mixin_not_implemented():
+    """Test NotImplementedError in ManagersMixin."""
+    mgrs = DummyManagers()
+    with pytest.raises(NotImplementedError):
+        mgrs._version_check("1.0.0")
+    with pytest.raises(NotImplementedError):
+        await mgrs.process_request("http://test")
+
+
+@pytest.mark.asyncio
+async def test_properties_mixin_not_implemented():
+    """Test NotImplementedError in PropertiesMixin."""
+    props = DummyProperties()
+    with pytest.raises(NotImplementedError):
+        props._version_check("1.0.0")
+    with pytest.raises(NotImplementedError):
+        await props.list_claims()
+    with pytest.raises(NotImplementedError):
+        await props.get_override()
+
+
+@pytest.mark.asyncio
+async def test_sensors_mixin_not_implemented():
+    """Test NotImplementedError in SensorsMixin."""
+    sensors = DummySensors()
+    with pytest.raises(NotImplementedError):
+        sensors._version_check("1.0.0")
+    with pytest.raises(NotImplementedError):
+        await sensors.process_request("http://test")
